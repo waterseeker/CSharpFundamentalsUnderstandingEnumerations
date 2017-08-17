45d024ae6e35ffcb501436df0102f0696a503ae7
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-17 07:57:53 -0500

    added some notes and a custom enum

--- CSharpFundamentalsUnderstandingEnumerati/Program.cs
+++ CSharpFundamentalsUnderstandingEnumerati/Program.cs
@@ -15,8 +15,51 @@ namespace CSharpFundamentalsUnderstandingEnumerati
       
 
             Console.Title = "Welcome to the friggin' console."; //set title of console window
-            Console.WriteLine("Hello world!");
+
+            //Console.WriteLine("Hello world!");
+
+
+            Console.WriteLine("Type in a superhero's name to see his nickname: ");
+            string userValue = Console.ReadLine();
+
+            SuperHero myValue;
+
+            //try to convert user input into an enum from a string
+            if (Enum.TryParse<SuperHero>(userValue, true, out myValue))
+
+                //3 params in tryparse
+                //userValue is what you are evaluating
+                //true is whether we are going to ignore capitalization
+                //out myValue  the output paramater that is sent from the method to the caller. This differes from 
+                        //a return statement in that the out will return the result of the operation (true or false in this case) and also
+                        //the value of the output param (in this case the value of myValue)
+            {
+
+                switch (myValue)
+                {
+                    case SuperHero.Batman:
+                        break;
+                    case SuperHero.Superman:
+                        break;
+                    case SuperHero.GreenLantern:
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+
+
             Console.ReadLine();
         }
     }
+
+    enum SuperHero
+    {
+        Batman,
+        Superman,
+        GreenLantern
+    }
+
 }
